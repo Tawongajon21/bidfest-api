259f6d894f6fcaa84041c7d9877a911eef2a1e2e
Tawonga021 <reginaldjongwe@gmail.com>
2025-10-07 14:18:56 +0200

    Update The Upload Middleware

--- routes/auction.js
+++ routes/auction.js
@@ -8,7 +8,16 @@ const uploadMiddleware = multer({ dest: 'uploads/' });
 
 const isAuth = require('../middlewares/auth');
 const auctionRoutes= express.Router();
+const storage=multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,"./uploads/")
+     
 
+    },
+    filename:(req,file,cb)=>{
+        cb(null,Date.now()+file.originalname);
+    }
+})
 const upload=multer({
     storage,
     limits:{
